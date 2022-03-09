00d55dd223a750dd71638f77dacc2be56dff8b13
thota007 <62298363+thota007@users.noreply.github.com>
2022-03-09 04:06:40 -0700

    prepared components to implement slider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import L from 'leaflet';
+import { Slider } from '@material-ui/core';
+
+
 
 function App() {
+
+
+  useEffect(() => {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var marker = L.marker([51.5, -0.09]).addTo(map)
+      .bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();   
+    
+  });
+
   return (
-<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
-  );
+    <div className='App'>
+    
+
+      <div id="map" style={{ width: "100vw", height: "1000px" }}></div>
+
+      
+      
+    </div>
+
+  )
 }
 
-export default App;
+export default App
+
+
+

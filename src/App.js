e260fc51639ba92fd3dd3837769f7388d78f614c
sspathare97 <sspathare97@gmail.com>
2022-03-26 03:22:37 -0600

    added markers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Component } from "react";
+
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 import './App.css';
 import YearSlider from "./YearSlider.js";
 
-function App() {
-  return (
-    <div>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-      <YearSlider />
-    </div>
-  );
+const getData = (year, month) => {
+  return fetch(`sample-responses/${year}/${month}.json`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }
+  )
+    .then(res => res.json());
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      year: 2020,
+      month: 1,
+      articles: [] 
+    };
+  }
+
+  componentDidMount() {
+    getData(this.state.year, this.state.month)
+    .then(res => this.setState({ articles: res.rows }))
+    .then(()=> console.log([this.state.articles[0].lng, this.state.articles[0].lat]));
+  }
+
+  render() {
+    if (this.state.articles && this.state.articles.length > 0) {
+      console.log([]);
+    }
+    return (
+      <div>
+        <MapContainer center={[20, 0]} zoom={3} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {this.state.articles.map(article => (
+            <Marker position={[article.lng, article.lat]} />
+          ))}
+        </MapContainer>
+        <YearSlider />
+      </div>
+    );
+  }
 }
 
 export default App;

4d232220e2eb8d76a0b14f5909819332009dd3f6
sspathare97 <sspathare97@gmail.com>
2022-03-26 03:57:49 -0600

    sidebar grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
-
-import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import './App.css';
 import YearSlider from "./YearSlider.js";
 
@@ -19,17 +20,17 @@ const getData = (year, month) => {
 class App extends Component {
   constructor() {
     super();
-    this.state = { 
+    this.state = {
       year: 2020,
       month: 1,
-      articles: [] 
+      articles: []
     };
     this.handleYearChange = this.handleYearChange.bind(this);
   }
 
   updateArticles() {
     getData(this.state.year, this.state.month)
-    .then(res => this.setState({ articles: res.rows }));
+      .then(res => this.setState({ articles: res.rows }));
   }
 
   componentDidMount() {
@@ -43,22 +44,31 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <MapContainer center={[20, 0]} zoom={3} scrollWheelZoom={true}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          {this.state.articles.map(article => (
-            <Marker key={article.webURL} position={[article.lng, article.lat]} />
-          ))}
-        </MapContainer>
-        <YearSlider 
-          year={this.state.year}
-          month={this.state.month} 
-          handleYearChange={this.handleYearChange}
-        />
-      </div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container>
+          <Grid item xs={3}>
+            Hi
+          </Grid>
+          <Grid item xs={9}>
+            <div style={{ position: 'relative' }}>
+              <MapContainer center={[20, 0]} zoom={3} scrollWheelZoom={true}>
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {this.state.articles.map(article => (
+                  <Marker key={article.webURL} position={[article.lng, article.lat]} />
+                ))}
+              </MapContainer>
+              <YearSlider
+                year={this.state.year}
+                month={this.state.month}
+                handleYearChange={this.handleYearChange}
+              />
+            </div>
+          </Grid>
+        </Grid>
+      </Box>
     );
   }
 }

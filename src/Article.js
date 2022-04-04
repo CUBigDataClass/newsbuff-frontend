8635d0a081477aa4a9b5695084bbaa0e85206e4e
thota007 <62298363+thota007@users.noreply.github.com>
2022-04-04 00:13:12 -0600

    updated article card content

--- src/Article.js
+++ src/Article.js
@@ -5,16 +5,27 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import moment from 'moment';
 
 export default class Article extends React.Component {
     getCardText(headline, description) {
         return description.length < headline.length ? headline : description;
+    }
+    getSection(section) {
+        return    (section!=='null' ? section : '');
+    }
+    getSubSection(subsection) {
+        return    (subsection!=='null' ? subsection : '');
     }
 
     render() {
         return (
             <Card sx={{ m: 2 }}>
                 <CardActionArea href={this.props.article.webURL} target="_blank">
+                <Typography variant="body2" color="text.secondary">
+                            {this.getSection(this.props.article.section)}  <br/>
+                            {this.getSubSection(this.props.article.subsection)}
+                        </Typography>
                     <CardMedia
                         component="img"
                         height="160"
@@ -27,7 +38,7 @@ export default class Article extends React.Component {
                         <Typography variant="body2" color="text.secondary">
                             {this.getCardText(this.props.article.headline, this.props.article.description)}
                         </Typography> <br/>
-                        <Typography variant="body2" color="text.secondary"> {this.props.article.location + ', ' + this.props.article.datetime}</Typography>
+                        <Typography variant="body2" color="text.secondary"> {this.props.article.location + ', ' + moment(this.props.article.datetime).format('MMMM Do [at] h:mm:ss a')}</Typography><br/>
                     </CardContent>
                 </CardActionArea>
             </Card>

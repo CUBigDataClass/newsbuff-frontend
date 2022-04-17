46c9cc6ef59f46eae860a6d491a7260f59eba090
sspathare97 <sspathare97@gmail.com>
2022-04-16 19:00:48 -0600

    updated center

--- src/App.js
+++ src/App.js
@@ -133,7 +133,7 @@ class App extends Component {
           </Grid>
           <Grid item xs={9}>
             <div style={{ position: 'relative' }}>
-              <MapContainer center={[20, 0]} zoom={3} scrollWheelZoom={true}>
+              <MapContainer center={[0, 0]} zoom={2} scrollWheelZoom={true}>
                 <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
